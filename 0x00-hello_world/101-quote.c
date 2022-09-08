3a1666b70bcfeebc6eb78927f853e8526fd83213
Umar Faruq Zubairu <umarfaruqzubairu@gmail.com>
2022-09-08 11:56:36 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Return:1
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
 
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	write(2, ch, sizeof(ch));
-
-	return (1);
+			return (1);
 
 }
